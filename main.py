80df7a37bdb9e328794e64cb71381e39ebaae4bb
Elisa Maria da Silva <elisams20@gmail.com>
2025-10-12 17:07:25 -0300

    Fix: Enabled CORS for all origins in main.py to solve Vercel connection issue

--- main.py
+++ main.py
@@ -28,11 +28,12 @@ origins = [
     "http://localhost:5173",
     "http://127.0.0.1:5173",
     "http://localhost:8000",
+    "https://analytics-frontend-react.vercel.app", 
 ]
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
